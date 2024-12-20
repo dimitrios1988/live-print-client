32cd0af8e33a2628bdaa28a9b06de2938588bdda
Dimitrios Chondrokoukis <dimitrios1988@msn.com>
2024-12-20 12:29:49 +0200

    Add authentication guard and login route to application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
-import { PrintUiComponent } from './print-ui/print-ui.component';
 import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'main', pathMatch: 'full' }, // Redirect empty path to 'home'
-  { path: 'main', component: MainComponent }, // Route for 'home'
+  { path: 'main', component: MainComponent, canActivate: [AuthGuard] }, // Route for 'home'
+  { path: 'login', component: LoginComponent }, // Route for 'login'
   { path: '**', redirectTo: 'main' }, // Redirect all unknown paths to 'home'
 ];
