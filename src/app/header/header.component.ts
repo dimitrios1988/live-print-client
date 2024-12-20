22fad87b53241b50496fab730713cb7ae7bb0bd8
Dimitrios Chondrokoukis <dimitrios1988@msn.com>
2024-12-20 13:05:40 +0200

    Add logout functionality and settings menu to header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,22 +4,28 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog } from '@angular/material/dialog';
 import { SettingsDialogComponent } from './settings-dialog/settings-dialog.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { LoginService } from '../login/login.service';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatMenuModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
   private readonly dialog: MatDialog;
 
-  constructor() {
+  constructor(private loginserivce: LoginService) {
     this.dialog = inject(MatDialog);
   }
 
   onSettingsClicked() {
     this.displaySettingsDialog();
+  }
+
+  onLogoutClicked() {
+    this.loginserivce.logout();
   }
 
   displaySettingsDialog() {
