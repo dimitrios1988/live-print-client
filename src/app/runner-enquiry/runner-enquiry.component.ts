9fc03174ecb3c0a00e3c27967b096cef7125c41a
Dimitrios Chondrokoukis <dimitrios1988@msn.com>
2026-03-14 10:01:32 +0200

    Increase printing timeout from 2500ms to 3000ms in RunnerEnquiryComponent

--- src/app/runner-enquiry/runner-enquiry.component.ts
+++ src/app/runner-enquiry/runner-enquiry.component.ts
@@ -213,7 +213,7 @@ export class RunnerEnquiryComponent {
               ) {
                 setTimeout(() => {
                   this.printButton?._elementRef.nativeElement.click();
-                }, 2500);
+                }, 3000);
               }
             } else {
               if (this.raceNumberInput) {
