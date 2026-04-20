29a29fac957474a1a2243369d135121e176749fa
Dimitrios Chondrokoukis <dimitrios1988@msn.com>
2026-04-20 17:02:12 +0300

    Rmoved provideAnimationsAsync

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,6 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './auth.interceptor';
 import { unauthorizedInterceptor } from './unauthorized.interceptor';
@@ -13,7 +12,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withHashLocation()),
-    provideAnimationsAsync(),
     provideHttpClient(
       withInterceptors([
         authInterceptor,
